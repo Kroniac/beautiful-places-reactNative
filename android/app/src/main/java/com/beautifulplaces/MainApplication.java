fa02a3ea5e7c8cbb15a21aabcaf27f955dbff3cd
Farid Ansari <farid12ansari7@gmail.com>
2018-01-20 20:37:16 +0530

    added react_native_pickImage

--- android/app/src/main/java/com/beautifulplaces/MainApplication.java
+++ android/app/src/main/java/com/beautifulplaces/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.soloader.SoLoader;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.imagepicker.ImagePickerPackage;
 
 
 import java.util.Arrays;
@@ -28,7 +29,8 @@ import java.util.List;
          // No need to add RnnPackage and MainReactPackage
          return Arrays.<ReactPackage>asList(
             new VectorIconsPackage(),
-            new MapsPackage()
+            new MapsPackage(),
+            new ImagePickerPackage()
          );
      }
 
